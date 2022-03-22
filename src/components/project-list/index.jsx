f87c134507a3dff15700ed32ad576dc7db5239fe
langmeng <langmeng@zuoyebang.com>
2022-03-22 20:31:32 +0800

    feat：暂存

--- src/components/project-list/index.jsx
+++ src/components/project-list/index.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {List} from './list';
 import {SearchPanel} from "./search-panel";
+
+const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectList = () => {
     const [param, setParam] = useState({
         name: '',
         personId: ''
     });
+    const [users, setUsers] = useState([]);
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        console.log('useEffect111=== ', param);
+        fetch(`${apiUrl}/projects`).then(async response => {
+            if (response.ok) {
+                setList(await response.json());
+            }
+        });
+    }, [param]);
+
+    useEffect(() => {
+        console.log('useEffect222=== ', param);
+        fetch(`${apiUrl}/users`).then(async response => {
+            if (response.ok) {
+                setUsers(await response.json());
+            }
+        });
+    }, []);
     return (
         <React.Fragment>
-            <SearchPanel param={param} setParam={setParam} />
-            <List/>
+            <SearchPanel param={param} setParam={setParam} users={users} setUsers={setUsers} />
+            <List list={list} users={users} />
         </React.Fragment>
     );
 };
--- src/components/project-list/search-panel.jsx
+++ src/components/project-list/search-panel.jsx
@@ -1,7 +1,15 @@
-export const SearchPanel = ({param, setParam}) => {
+import React from 'react';
+
+export const SearchPanel = ({param, setParam, setUsers, users }) => {
     return (
         <form action=''>
             <input value={param.name} onChange={e => setParam({ ...param, name: e.target.value })} />
+            <select value={param.personId} onChange={e => setParam({ ...param, personId: e.target.value })}>
+                <option>负责人</option>
+                {
+                    users.map(user => <option key={user.id}>{user.name}</option>)
+                }
+            </select>
         </form>
     );
 };
